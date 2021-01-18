0a4111fec6a6c43293c189fdd4e0fa9b466a75fe
mike-jshen <68671792+mike-jshen@users.noreply.github.com>
2021-01-18 12:05:03 -0500

    double check

--- TAB2MXL/src/main/java/TAB2MXL/Main.java
+++ TAB2MXL/src/main/java/TAB2MXL/Main.java
@@ -5,11 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to 2311 Project !! \n And JAVA SE - 12");
-<<<<<<< HEAD
 		System.out.println("This is mike's change");
 		
-=======
 		testingVar = 0;
->>>>>>> branch 'main' of https://github.com/mike-jshen/2311-Project
 	}
 }
