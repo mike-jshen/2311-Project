fcace6f4c42e8f4ca7d94828835f74576e327e4a
Maksim Kolotev <maxrock@my.yorku.ca>
2021-01-23 14:15:25 -0500

    BASIC READER
    This is the basic file reader for now, it does nothing but read the file
    than but it out onto Java, You must enter the text file name on "ENTER
    FILE NAME" to test it. Not sure if it works just properly yet

--- MusicXML/src/main/java/MusicXML/Library.java
+++ MusicXML/src/main/java/MusicXML/Library.java
@@ -3,7 +3,45 @@
  */
 package MusicXML;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.*;
+import java.io.File;
+
+//Not all of the above are needed, they are there just in case.
+
 public class Library {
+	
+	public static void main (String[] args) throws Exception{
+		
+		// This is the file you want to read
+		File file = new File("ENTER FILE NAME");
+		
+		try {
+			// This is the scanner that actually reads the file
+			Scanner reader = new Scanner (file);
+			
+			// This is code is the one that actually prints it out into java
+			while (reader.hasNextLine()) {
+				
+				String data = reader.nextLine();
+				System.out.println(data);
+				
+			}
+			
+			//closes reader
+			reader.close();
+		}
+		// catches any exceptions
+		catch (IOException e) {
+			System.out.println("There was an error");
+			e.printStackTrace();
+		}
+		
+	}
+	
     public boolean someLibraryMethod(){
     	int zero = 0;
         return true;
