7189d2ab8320f5e73dc8d02d1b3e6c22b7f7e162
mike-jshen <68671792+mike-jshen@users.noreply.github.com>
2021-01-28 01:58:27 -0500

    Redesigned the main class to be more object oriented. (Added FileScanner class)

--- MusicXML/src/main/java/MusicXML/Library.java
+++ MusicXML/src/main/java/MusicXML/Library.java
@@ -19,60 +19,30 @@ public class Library {
 		
 
 		File file = new File("tab1.txt");
-		ArrayList <String> lines = new ArrayList<String>();
-		
-		
-		try {
-			// This is the scanner that actually reads the file
-			Scanner reader = new Scanner (file);
-			
-			// This is code is the one that actually prints it out into java
-			while (reader.hasNextLine()) {
-				
-				String data = reader.nextLine();
-				System.out.println(data);
-				lines.add(data);
-			}
-			//closes reader
-			reader.close();
-			
+		FileScanner read = new FileScanner(file);
 			// ------------------------------------------------------------------------------------------------------------
-			// find spaces between bars
-			char[] array;
-			array = lines.get(0).toCharArray();
-			System.out.println("Length of one line: " + array.length);
+			// find spaces between bars (Consult FileScanner Class)
+			System.out.println("Length of one line: " + read.FirstLine());
+						
+			read.SpaceCounter(); // Count the number of spaces in each bar & count how many bars there are (Evokes void method)
 			
-			int barCounter = 0;
-			int spacesBetweenBar = 0;
-			
-			for(int i = 1; i < array.length; i++) { 			// int i starts at 1 to skip first character, the key of the string
-				if(barCounter < 2) {
-					if(array[i] == '|') {
-						barCounter++;
-					}
-					else {
-						spacesBetweenBar++;
-					}
-				}
-			}
-			
-			System.out.println("Number of spaces in the first bar: " + spacesBetweenBar);		
+			System.out.println("Number of spaces in the first bar: " + read.spacesBetweenBar);		
 			
 			// one problem that might arise, user might have different spacing for the bars (e.g. first bar has 26 spaces, second has 25)
 			// ------------------------------------------------------------------------------------------------------------
 			// parse bars into string arrays
 			
-			String firstLine = lines.get(1); 			// change this to move between guitar strings (e.g. index 0 is thinnest string)
+			String firstLine = read.lines.get(1); 			// change this to move between guitar strings (e.g. index 0 is thinnest string)
 			String lineWithoutKey = firstLine.substring(2);
 			
 			String[] bars = lineWithoutKey.split("\\|");
 			
 			for (String bar : bars)
 				System.out.println(bar);
-			
-			// ------------------------------------------------------------------------------------------------------------
-			// identify numbers at certain index
-			
+//			
+//			// ------------------------------------------------------------------------------------------------------------
+//			// identify numbers at certain index
+//			
 			char[] firstBar = bars[0].toCharArray();
 			
 			for(int i = 0; i < firstBar.length; i++) {
@@ -85,17 +55,10 @@ public class Library {
 			
 		}
 		// catches any exceptions
-		catch (IOException e) {
-			System.out.println("There was an Error");
-			e.printStackTrace();
-		}
-		
-	}
+
 	
     public boolean someLibraryMethod(){
-    	int zero = 3;
+    	int zero = 0; // Used for TA Demo. Dunno what else this is used for (Maybe created by default???)
         return true;
-
-
     }
 }
