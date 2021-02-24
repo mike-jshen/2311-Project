c13be5068098f875f968b03e11103fdb3488c6b1
Mohamed Ahmed <moe.ahmed74@gmai.com>
2021-02-24 13:32:30 -0500

    Created build.gradle and deleted all test cases.

--- MusicXML/src/main/java/testSuite/AllTests.java
+++ MusicXML/src/main/java/testSuite/AllTests.java
@@ -1,11 +1,1 @@
 package testSuite;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({})
-public class AllTests {
-
-}
